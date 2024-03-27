a5991f070eb536df9863b3d68c5adc8499021564
arachne1102 <s1284123@gmail.com>
2024-03-27 17:17:26 +0900

    commit

---
+++ src/test/kotlin/com/daelim/springtest/CoroutineQuiz.kt
@@ -0,0 +1,71 @@
+package com.daelim.springtest
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import net.datafaker.Faker
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import java.util.*
+
+@SpringBootTest
+class CoroutineQuiz {
+
+    @Test
+    fun testDataFaker(){
+        val faker = Faker(Locale.KOREA)
+        println(faker.name().name())
+    }
+
+    /*
+        100명의 가상 사용자 이름을 로그로 출력하세요
+    */
+    @Test
+    fun quiz1Test(){
+        val faker = Faker(Locale.KOREA)
+        runBlocking {
+            List(100) {
+                println(faker.name().name())
+            }
+            println("finish")
+        }
+
+
+    }
+
+    /*
+        50명의 가상 사용자의 이름, 이메일, 주소를 로그 출력하세요
+     */
+    @Test
+    fun quiz2Test(){
+        val faker = Faker(Locale.KOREA)
+        runBlocking {
+            List(50) {
+                println("${faker.name().name()}, ${faker.internet().emailAddress()}, ${faker.address().fullAddress()}")
+            }
+            println("finish")
+        }
+    }
+
+    /*
+        30명의 가상 사용자의 이름과 나이 생성를 데이터클래스로 생성하고, 어린 나이 순으로 정렬 후 출력하세요
+     */
+    data class User(val name: String, val age: Int)
+
+    @Test
+    fun generateSortAndPrintUserAges() {
+        runBlocking {
+            val faker = Faker(Locale.KOREA)
+            val users = List(30) {
+                async {
+                    User(
+                        name = faker.name().fullName(),
+                        age = faker.number().numberBetween(18, 60)
+                    )
+                }
+            }
+            users.map { it.await() }.sortedBy { it.age }.map {
+                println("${it.name}, ${it.age}")
+            }
+        }
+    }
+}
